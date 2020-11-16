057dfeae4f6941016eea05f362f8b4389ec384a4
Andrea Tredrea <andrea.tredrea@gmail.com>
2020-11-16 21:10:21 +0100

    more JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,21 +21,7 @@ require("channels")
 // Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
 // WRITE YOUR OWN JS STARTING FROM HERE 👇
 // ----------------------------------------------------
-function myFunction() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
 
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less";
-    moreText.style.display = "inline";
-  }
-}
 // External imports
 import "bootstrap";
 
